2021a8328833a300efd32d04b45b5d755b68921f
vsafonova <viktoriyablak@gmail.com>
2023-11-10 10:56:50 +0100

    Delete scripts files which I don't  need

--- script.js
+++ script.js
@@ -1,32 +1,113 @@
-'use strict'
+"use strict";
 
-// computer make up random number 1-100
+let randomNumber;
+let guessesCounter; // Variable for amount of guesses left
 
-let randomNumber = Math.floor(Math.random()*100)+1;
+let guessButtonEl = document.querySelector("#form");
+guessButtonEl.addEventListener("submit", guess);
 
-let userGuess;
+let cancelButtonEl = document.querySelector("#cancel");
+cancelButtonEl.addEventListener("click", resetGame);
 
-/*Use binary search (Firstly find the middle of numbers.
-You'll get an alert with a random number less or higher than the user's guess. Then repeat this algorithm until you find the correct number) */
+let playAgainButtonEl = document.querySelector("#playagain");
+playAgainButtonEl.addEventListener("click", resetGame);
 
-while(userGuess !== randomNumber) {
+let gameFieldEl = document.querySelector("#gamefield");
+const imageEl = document.getElementById("image");
+const instructionsEl = document.querySelector("#instructions");
+const congratsEl = document.querySelector("#congratulations");
 
-   const userInput = prompt('Try to guess the number between 1 and 100:')
+resetGame(); //Initialize game
 
-   userGuess = Number(userInput);
-
-   if (userInput === null) {
-      break
-   }
-
-   if (userGuess > randomNumber) {
-      alert('Too high. Try again!')
-   } else if(userGuess < randomNumber) {
-      alert('Too low. Try again!')
-   } 
+//Generate random number between 1-100
+function setRundomNumber() {
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber);
 }
 
-if(userGuess === randomNumber) {
-   alert('Congratulations! You guessed the correct number!');
+//Compare value with random number
+function checkGuess(userGuess) {
+  if (userGuess === randomNumber) {
+    win();
+    return true;
+  } else if (userGuess < randomNumber) {
+    alert("Too low. Try again!");
+    return false;
+  } else if (userGuess > randomNumber) {
+    alert("Too high. Try again!");
+    return false;
+  }
 }
 
+//When your tries are finished - finish the game
+function loss() {
+  imageEl.src = "https://media.giphy.com/media/qtgkm1xVilBFWD63CT/giphy.gif";
+  endGame();
+}
+
+//Get value from input
+function guess(event) {
+  event.preventDefault(); // Prevent the form from actually submitting
+
+  // Get the form data using FormData
+  const formData = new FormData(event.target);
+
+  // You can now access and process the form data as needed
+  const value = Number(formData.get("userGuess"));
+
+  let inputEl = document.querySelector("#userGuess");
+
+  inputEl.value = ""; //reset input after entering a number
+
+  decreaseCounter();
+  let isCorrectGuess = checkGuess(value);
+
+  if (guessesCounter <= 0 && !isCorrectGuess) {
+    loss();
+  }
+}
+
+//Show many tries you have
+function showCounter() {
+  let countLeftTries = document.querySelector("#tries");
+  countLeftTries.innerText = `You have ${guessesCounter} tries`;
+}
+
+//Decrease number of tries remaining
+function decreaseCounter() {
+  setGuesses(guessesCounter - 1);
+}
+
+// After click on "cancel" abort the game even if I still have tries
+function resetGame() {
+  setRundomNumber();
+  setGuesses(7);
+
+  playAgainButtonEl.style.display = "none";
+  gameFieldEl.style.display = "flex";
+  imageEl.src = "./images/image.jpg";
+  instructionsEl.style.display = "flex";
+  congratsEl.style.display = "none";
+}
+
+//Set counter of tries
+function setGuesses(guesses) {
+  if (guesses >= 0) {
+    guessesCounter = guesses;
+    showCounter();
+  }
+}
+
+//What is happening if you won
+function win() {
+  imageEl.src = "https://media.giphy.com/media/BPJmthQ3YRwD6QqcVD/giphy.gif";
+  endGame();
+  congratsEl.style.display = "block";
+}
+
+//What is happening when you won or loss
+function endGame() {
+  playAgainButtonEl.style.display = "block";
+  gameFieldEl.style.display = "none";
+  instructionsEl.style.display = "none";
+}
